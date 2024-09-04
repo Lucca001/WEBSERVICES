934d73c6cff73358ae0004f46c057af5b11e22c1
Devlucca <108484619+Lucca001@users.noreply.github.com>
2024-09-04 07:11:37 +0000

    Pending changes exported from your codespace

---
+++ .gitignore/maven.apache.org/POM/src/test/java/br/com/fiap/health4me/Health4meApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.health4me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Health4meApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
